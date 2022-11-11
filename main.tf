58094594d455b6f65479469154e60ae5eda3867d
pzohaycuoi <pzohaycuoi@gmail.com>
2022-11-11 18:34:43 +0700

    remove ec2 module

--- main.tf
+++ main.tf
@@ -13,11 +13,6 @@ provider "aws" {
   region = var.region
 }
 
-module "nambeoec2" {
-  source = "app.terraform.io/nambeotest/nambeoec2/aws"
-  subnet = nambeovpc.private_subnets
-}
-
 module "namebeovpc" {
   source = "app.terraform.io/nambeotest/nambeovpc/aws"
   private_subnet_range = var.private_subnet_range
