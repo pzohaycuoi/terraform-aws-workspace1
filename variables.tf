94d89e976d92afbac91927789bc89dc301ada30c
pzohaycuoi <pzohaycuoi@gmail.com>
2022-11-11 23:20:16 +0700

    get rid of private subnet var

--- variables.tf
+++ variables.tf
@@ -4,11 +4,6 @@ variable "region" {
   default = "ap-southeast-1"
 }
 
-variable "private_subnet_range" {
-  description = "private subnet ip range"
-  type = list(any)
-}
-
 variable "public_subnet_range" {
   description = "public subnet ip range"
   type = list(any)
