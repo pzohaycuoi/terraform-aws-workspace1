6524aae362f7b66efb79581b4e64e4a27d633764
pzohaycuoi <pzohaycuoi@gmail.com>
2022-11-12 23:08:48 +0700

    fix output from root module

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = "${module.vpc.vpc_id}"
 }
 
 output "private_subnet" {
-  value = module.vpc.private_subnets
+  value = "${module.vpc.private_subnets}"
 }
 
 output "public_subnet" {
-  value = module.vpc.public_subnets
+  value = "${module.vpc.public_subnets}"
 }
 
 output "region" {
