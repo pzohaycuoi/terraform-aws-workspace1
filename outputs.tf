0abaa9dcd28e7278816582e0792d642d42d9434a
pzohaycuoi <pzohaycuoi@gmail.com>
2022-11-12 23:57:57 +0700

    add provider fix

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,6 @@ output "vpc_id" {
   value = module.nambeovpc.vpc_id
 }
 
-output "private_subnet" {
-  value = module.nambeovpc.private_subnet
-}
-
-output "public_subnet" {
-  value = module.nambeovpc.public_subnet
-}
-
 output "region" {
   value = var.region
 }
